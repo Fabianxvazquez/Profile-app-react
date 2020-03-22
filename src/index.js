fb8698d43ea7462be0357d2495794eec40475d6c
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-03-21 20:05:00 -0600

    added navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
@@ -10,9 +10,11 @@ import { BrowserRouter, } from "react-router-dom";
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>, 
   document.getElementById('root')
 );
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
