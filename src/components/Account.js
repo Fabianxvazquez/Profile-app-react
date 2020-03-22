bc250d12ac6b88f25be3ee3f21b8ecdfd8f21648
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-03-22 13:18:30 -0600

    added account.js

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,17 +1,23 @@
 import React from "react"
 import {Card, } from "semantic-ui-react"
+import {AccountConsumer, } from "../providers/AccountProvider"
 
 const Account = () => (
-  <Card>
-    <Card.Content>
-      <Card.Header> firstName lastName </Card.Header>
-      <Card.Meta>
-        Date Joined: dateJoined
-      </Card.Meta>
-    </Card.Content>
-    <Card.Content>
-      <p>email: email@email.com</p>
-    </Card.Content>
-  </Card>
+  <AccountConsumer>
+    { value => (
+      <Card>
+      <Card.Content>
+        <Card.Header>{value.firstName} {value.lastName} </Card.Header>
+        <Card.Meta>
+          Date Joined: {value.dateJoined}
+        </Card.Meta>
+      </Card.Content>
+      <Card.Content>
+        <p>Email: {value.email}</p>
+      </Card.Content>
+    </Card>
+    )}
+  
+  </AccountConsumer>
 ) 
 export default Account
